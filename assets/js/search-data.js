ca7497324f4349937d101cdc5e8f0aa974a71b6d
k-karakatsanis <dinoskarakas@gmail.com>
2025-04-03 22:03:27 +0000

    Deploying to gh-pages from @ k-karakatsanis/k-karakatsanis.github.io@b311821bfe78b5a7fda1bc46ff39d7179b49001a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "books-the-godfather",
+  },{id: "nav-publications",
+          title: "publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/publications/";
+          },
+        },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
