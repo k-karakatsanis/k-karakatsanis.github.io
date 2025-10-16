08dbe53b3d25a11841482321cef35e4c15320559
k-karakatsanis <dinoskarakas@gmail.com>
2025-10-16 21:22:06 +0000

    Deploying to gh-pages from @ k-karakatsanis/k-karakatsanis.github.io@2f9e3f26b4f72c891eabb1876a517094cad55e4a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,16 +28,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-paper-pytrim-a-practical-tool-for-reducing-python-dependency-bloat-has-been-accepted-at-ase-2025-tool-demonstration-track",
+          title: 'Our paper PyTrim: A Practical Tool for Reducing Python Dependency Bloat has been...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
